c537cd82fa60b2be0ef49dd7ac6530aa5a5fb36e
Ansh070 <gargansh749@gmail.com>
2026-01-20 10:21:45 +0530

    add database

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class ToDo(db.Model):
+    SNo = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"<ToDo {self.SNo} - {self.title}>"
 @app.route('/')
 def home():
     return render_template('index.html')
